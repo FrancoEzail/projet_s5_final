775b7e978e6451c923e749199a25523bf0ec5e00
FrancoEzail <francoezail@gmail.com>
2024-01-29 18:04:53 +0300

    rollback

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import { BrowserRouter as Router, Route,BrowserRouter, Routes } from 'react-router-dom';
-import Login from './page/Login';
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-        <Route path='/' exact element={<Login />} />
-        <Route path='/login' exact element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
